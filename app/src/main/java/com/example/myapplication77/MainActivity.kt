d3a7cff4439b61ae1e894659530abb044e15f193
krasnovss91 <krasnovstanislav91@gmail.com>
2022-01-31 10:16:59 +0400

    views_1

--- app/src/main/java/com/example/myapplication77/MainActivity.kt
+++ app/src/main/java/com/example/myapplication77/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication77
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View;
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun sendMessage(view: View){
+      val textView = findViewById<TextView>(R.id.text)
+      val editText = findViewById<EditText>(R.id.parent)
+        Log.d("text1", editText.text.toString())
     }
 }
