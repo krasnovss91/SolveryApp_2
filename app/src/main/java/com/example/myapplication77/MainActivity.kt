68b8ba5dad1229a04629f96239a53c2ae4af2d26
krasnovss91 <krasnovstanislav91@gmail.com>
2022-01-31 11:58:45 +0400

    views_3

--- app/src/main/java/com/example/myapplication77/MainActivity.kt
+++ app/src/main/java/com/example/myapplication77/MainActivity.kt
@@ -14,12 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.text)
-        val editText_1 = findViewById<EditText>(R.id.editTextTextPersonName1)
 
+
+        val editText_1 = findViewById<EditText>(R.id.editTextTextPersonName1)
+        val editText_2 = findViewById<EditText>(R.id.editTextTextPersonName2)
+
+        /*
         val button = findViewById<AppCompatButton>(R.id.parent)
         button.setOnClickListener {
              Log.d("text1",editText_1.text.toString())
+             Log.d("text2",editText_2.text.toString())
         }
+         */
     }
-
+   // https://stackoverflow.com/questions/19181362/stash-the-value-of-edittext-into-an-variable-and-print-it-into-logcat
 }
